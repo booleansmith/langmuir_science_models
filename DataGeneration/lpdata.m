1d1d2195fab4d6aed1cb4818cdf2434ddcd82a55
Jason Powell <32348565+booleansmith@users.noreply.github.com>
2022-09-12 16:16:10 -0600

    fixed equations (adjusted units), also started the curve fitting

--- DataGeneration/lpdata.m
+++ DataGeneration/lpdata.m
@@ -36,51 +36,97 @@
 % KNOWN CONSTANTS
 k_b = 1.38e-23;  % Boltsmann constant m^2*kg*s^-2*K^-1
 e   = 1.602e-19; % elementary charge
-q_e   = -1.602e-19; % electron charge coulombsq_e   = -1.602e-19; % electron charge coulombs
-q_i   = 1.602e-19;  % ion charge coulombs
-m_e = 5.486e-4; % electron mass, kg
+q_e   = e; % electron charge coulombs 
+q_i   = e;  % ion charge coulombs
+m_e = 9.109e-31; % electron mass, kg
 
 % Environment Parameters
 n = 1e12; % density m^-3
 T = 1160; % Temperature, K
-m_i = 16; % O+ ion mass
+m_i = 2.65e-26; % O+ ion mass, kg
 
-phi = -2:0.01:3; % Bias potential, Volts
 phi_p = 1; % plasma potential, Volts
-V_sc  = -7.4e3; % SC velocity, m/s
+phi = -2:0.01:phi_p; % Bias potential, Volts
+V_sc  = 7.4e3; % SC velocity, m/s
 
 
 % Probe parameters
 % probeRadius = 0.5e-3;    % meters
 % probeHeight = 85.2e-3; % meters
-probeRadius = 0.5;    % meters
-probeHeight = 85.2; % meters
+probeRadius = 0.5e-3;    % meters
+probeHeight = 85.2e-3; % meters
 A_proj = probeRadius*2*probeHeight; % projected area, m^2
 A      = pi*probeRadius*probeHeight; % OML collection area, m^2
 beta = 1;
 
-I_ram = n*A_proj*e*V_sc;
-I_th_e = n*q_e*A*sqrt((k_b*T)/(2*pi*m_e));
-I_th_i = n*q_i*A*sqrt((k_b*T)/(2*pi*m_i));
+I_ram  = n*A_proj*e*V_sc;
+I_th_e = n*e*A*sqrt((k_b*T)/(2*pi*m_e));
+I_th_i = n*e*A*sqrt((k_b*T)/(2*pi*m_i));
 I_OML_i = I_th_i*(1-((e*(phi-phi_p))/(k_b*T))).^beta;
 I_e     = I_th_e*exp((e*(phi-phi_p))/(k_b*T));
 
 I = -I_ram -I_OML_i + I_e;
 
 figure
-tiledlayout('flow')
-nexttile
-plot(phi,-I)
-nexttile
-plot(phi,-I_OML_i)
-nexttile
-plot(phi,-I_e)
+plot(phi,I)
+xlabel('Probe Potential [V]')
+ylabel('Current out of probe [A]')
+title('Langmuir Probe IV curve')
 
-%%
+%% Solve for stage 1 parameters
+
+% floating potential
+vf = interp1(I,phi,0); % The voltage at the zero current threshold
+
+% fit a line to the ion current collection
+index = phi > -2 & phi < -1.5;
+p = polyfit(phi(index),I(index),1);
+I_ram_est = -polyval(p,phi_p);
+n_est = I_ram_est/(A_proj*e*V_sc);
 
 figure
-plot(phi,-I_e)
+plot(phi,I)
+hold on
+plot(phi,polyval(p,phi))
+plot(phi_p,-I_ram_est,'r*')
+yline(-I_ram_est)
+xline(phi_p)
+text(phi_p+0.05,-I_ram_est+0.05,['I_{ram} = ', sprintf('%0.2f',I_ram_est)])
+xlim([-2,2])
+title('Ram Current Estimation')
 
+%% Do non-linear curve fitting
+
+% x(1): n - number density
+% x(2): T - temperature
+% x(3): phi_p - plasma potential
+% x(4): beta - OML parameter
+fun = @(x,phi) -x(1)*A_proj*e*V_sc ... % ion ram current
+                        -x(1)*e*A*sqrt((k_b*x(2))/(2*pi*m_i))*(1-((e*(phi-x(3)))/(k_b*x(2)))).^x(4) ... % ion current
+                        +x(1)*e*A*sqrt((k_b*x(2))/(2*pi*m_e))*exp((e*(phi-x(3)))/(k_b*x(2))); % electron current
+
+
+% for now we use known values for phi_p and beta
+T_guess = 1200; % kelvin
+phi_guess = phi_p;
+beta_guess = beta;
+
+x0 = [n_est,T_guess,phi_guess,beta_guess]; % initial guesses for lsqcurvefit
+bounds(1,:) = x0 - 0.05*x0;
+bounds(2,:) = x0 + 0.05*x0;
+tolerance = 1e-25;
+options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt','StepTolerance',tolerance,'FunctionTolerance',tolerance);
+x = lsqcurvefit(fun,x0,phi,I,bounds(1,:),bounds(2,:),options);
+
+
+figure
+tiledlayout(4,1)
+nexttile([3,1])
+plot(phi,I)
+hold on
+plot(phi,fun(x,phi))
+nexttile
+plot(phi,abs(I-fun(x,phi)))
 
 
 
