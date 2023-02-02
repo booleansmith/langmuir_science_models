8734d102716a62ec3396bcf1783e382c2ab43a92
Jason Powell <32348565+booleansmith@users.noreply.github.com>
2023-02-02 16:47:51 -0700

    created functions to consider SPORT simulation

---
+++ Analysis/gettempdensityest2.m
@@ -0,0 +1,210 @@
+function [t,n] = gettempdensityest2(V,I,fpp,dodebug,temp,density)
+% Function NAME:
+%
+%   Get Temperature and Density Estimation 2
+% 
+% DESCRIPTION:
+%
+%   Takes current voltage current measurements and returns temperature and
+%   denisty parameters. Version 2 consideres data from fpp's to use in
+%   calculating the floating potential of the spacecraft
+%
+% INPUTS:
+%
+%   V   - (double array) The bias potential applied to the spacecraft
+%   I   - (double array) The measured current for each biased potential
+%   fpp - (double array) The floating potential as measured by fpp
+%
+% OPTIONAL INPUTS
+%
+%   dodebug - (boolean) optionally enable plots for debugging
+%   temp    - (double) the actual temperature
+%   density - (double) the actual density
+%
+% OUTPUTS:
+%
+%   t - (double) the temperature estimate produced by function
+%   n - (double) the density estimate produced by the function
+% 
+% ASSUMPTIONS AND LIMITATIONS:
+%
+%   An oxygen ion plasma is assumed
+%
+% Jason Powell
+% Feb 02, 2023
+%%
+
+if ~exist('dodebug','var')
+    dodebug = false;
+end
+
+if ~exist('temp','var')
+    temp = 1200;
+    warning('Temperature data assumed')
+end
+
+if ~exist('density','var')
+    density = 1e12;
+    warning('Density data assumed')
+end
+
+
+% KNOWN CONSTANTS
+k_b = 1.38e-23;  % Boltsmann constant m^2*kg*s^-2*K^-1
+e   = 1.602e-19; % elementary charge
+m_e = 9.109e-31; % electron mass, kg
+m_i = 2.65e-26; % O+ ion mass, kg
+
+
+% Probe/ SC parameters
+probeRadius = 0.5e-3;    % meters
+probeHeight = 85.2e-3; % meters
+A_proj = probeRadius*2*probeHeight; % projected area, m^2
+% A      = 2*pi*probeRadius*probeHeight; % OML collection area, m^2
+A = 297.287e-6; % TODO: The actual area of SPORT LP needs to be decided
+Rp = 0.5e-3; % radius of the probe, m
+beta = 0.5;
+V_sc  = 7.4e3; % SC velocity, m/s
+
+
+
+%% Pre-treat the data a little bit
+% This is necessary to interpolate values out of IV curve, hopefully no
+% meaningful data is lost
+
+[I,ia,~] = unique(I,'stable');
+V = V(ia);
+
+%% Solve for stage 1 parameters
+
+% floating potential
+vf = interp1(I,V,0); % The voltage at the zero current threshold
+
+% plasma potential
+Idot = gradient(I,V); % di/dv
+phi_p = max(Idot(V > vf & V < vf+1.5));
+
+% ion density
+index = V > -2 & V < -1.5;
+p = polyfit(V(index),I(index),1);
+I_ram_est = -polyval(p,phi_p);
+% Note, when the sc does not have a velocity, this method will not work.
+V_sc = 3.0258e+03; 
+ni_est = I_ram_est/(A_proj*e*V_sc);
+
+
+if dodebug 
+    figure
+    plot(V,I)
+    hold on
+    plot(vf,0,"*",'MarkerSize',5)
+    plot(V,polyval(p,V),'--k','LineWidth',1)
+    xline(phi_p,'--k')
+    plot(phi_p,-I_ram_est,"*",'MarkerSize',5)
+    text(phi_p+.05,-I_ram_est-2.5e-7,sprintf('n_i = %0.2e',ni_est))
+    ylabel('Return Current (A)')
+    yyaxis right
+    plot(V,Idot)
+    hold on
+    plot(phi_p,Idot(Idot == phi_p),"*",'MarkerSize',5)
+    xlabel('Bias Potential (V)')
+    ylabel('dI/dV')
+
+    title('First Stage parameters from IV curve')
+    legend('I','Vf','','','I_{Ram}','dI/dV','\Phi_p')
+end
+
+%% Estimate the sheath potential
+
+maxFpp = max(fpp);
+
+
+
+%% Do non-linear curve fitting
+
+% x(1): n - number density
+% x(2): T - temperature
+% x(3): phi_p - plasma potential
+% x(4): beta - OML parameter
+% x(5): A - Area of the probe
+% x(6): Rp - Radius of the probe
+% fun = @(x,phi) -x(1)*A_proj*e*V_sc ... % ion ram current
+%                -x(1)*e*A*sqrt((k_b*x(2))/(2*pi*m_i))*(1-((e*phi)/(k_b*x(2)))).^x(4) ... % ion current
+%                -x(1)*-e*A*sqrt((k_b*x(2))/(2*pi*m_e))*exp((e*phi)/(k_b*x(2))); % electron current
+ 
+fun = @(x,phi) -x(1)*-e*A*sqrt((k_b*x(2))/(2*pi*m_e))*exp((e*phi)/(k_b*x(2))); % ion ram current
+
+
+% for now we use known values for phi_p and beta
+T_guess = 1250; % kelvin
+phi_guess = phi_p;
+% beta_guess = beta;
+beta = 0.5;
+
+% Ion saturation region/ Electron retradation region mask
+m = V < 0.01;
+x0 = [ni_est,T_guess,phi_guess,beta,A,Rp]; % initial guesses for lsqcurvefit
+bounds(1,:) = [ni_est - 0.5*ni_est, 300,phi_guess,beta, A, Rp];
+bounds(2,:) = [ni_est + 0.5*ni_est, 5000,phi_guess,beta, A, Rp];
+tolerance = 1e-25;
+options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt','StepTolerance',tolerance,'FunctionTolerance',tolerance);
+x1 = lsqcurvefit(fun,x0,V(m),I(m),bounds(1,:),bounds(2,:),options);
+x2 = lsqcurvefit(@OMLCurrentApprox,x0,V,I,bounds(1,:),bounds(2,:),options);
+x3 = lsqcurvefit(@OMLCurrentCyl,x0,V,I,bounds(1,:),bounds(2,:),options);
+
+xtrue = [density,temp,phi_guess,beta,A,Rp];
+
+
+if dodebug
+    
+    
+
+    figure
+    tiledlayout('flow')
+
+    nexttile % Debchoudhury
+    plot(V,I)
+    hold on
+    plot(V(m),fun(x1,V(m)))
+    plot(V(m),fun(xtrue,V(m)))
+    legend('LTSpice','Line Fit','Matlab','Location','northwest')
+    xlabel('Potential Across Sheath, \phi_s [V]')
+    ylabel('Current Collected by Probe [A]')
+    title('Debchoudhury')
+    str = sprintf('N_{est} = %0.2e, T_{est} = %0.2e', x1(1),x1(2));
+    text(0.75,0.5*mean(I),str);
+
+    nexttile % Swenson OML Approx
+    plot(V,I)
+    hold on
+    plot(V,OMLCurrentApprox(x2,V))
+    plot(V,OMLCurrentApprox(xtrue,V))
+    legend('LTSpice','Line Fit','Matlab','Location','northwest')
+    xlabel('Potential Across Sheath, \phi_s [V]')
+    ylabel('Current Collected by Probe [A]')
+    title('Swenson OML Approx')
+    str = sprintf('N_{est} = %0.2e, T_{est} = %0.2e', x2(1),x2(2));
+    text(0.75,0.5*mean(I),str);
+    
+    nexttile % Swenson OML
+    plot(V,I)
+    hold on
+    plot(V,OMLCurrentCyl(x3,V))
+    plot(V,OMLCurrentCyl(xtrue,V))
+    legend('LTSpice','Line Fit','Matlab','Location','northwest')
+    xlabel('Potential Across Sheath, \phi_s [V]')
+    ylabel('Current Collected by Probe [A]')
+    title('Swenson OML Cyl')
+    str = sprintf('N_{est} = %0.2e, T_{est} = %0.2e', x3(1),x3(2));
+    text(0.75,0.5*mean(I),str);
+
+    titlestring = sprintf('Actual Paramters: n = %0.2e, T = %0.2e',density,temp);
+    sgtitle(titlestring)
+    
+
+end
+t = [x1(2),x2(2),x3(2)];
+n = [x1(1),x2(1),x3(1)];
+
+end
+
---
+++ Analysis/interpretsportspice.m
@@ -0,0 +1,60 @@
+% SCRIPT NAME:
+%
+%   Interpret Tempature Simulation
+% 
+% DESCRIPTION:
+%
+%   This script is designed to look at a specific simulation done in
+%   LTspice, 'SPORTmodelV5.asc'. The Spice model considers the sport 
+%   spacecraft over different spacecraft areas. This data set is used 
+%   to verify methods for approximating temperature and density profiles 
+%   with function "gettempdensityest.m".
+%
+% Jason Powell
+% Feb 02, 2023
+
+addpath(fullfile('..','utilities'))
+boxpath = getboxpath;
+
+
+%% Load SPORTmodelV5 from .txt file, 
+% This only needs to be done once, but is nice to see demonstrate how to get the spice data as a matlab table
+
+sportData = fullfile(boxpath,"LTspice","langmuir","SPORTmodelV5.txt");
+data = loadspicefile(sportData);
+save(fullfile(boxpath,"LTspice","langmuir","SPORTmodelV5.mat"),"data")
+
+%% Plot the data
+
+areaarray = unique(data.steps);
+
+for i = 1:height(areaarray)
+    d2 = data(data.steps == areaarray(i),:);
+    figure
+    tiledlayout(2,1)
+    % current vs bias voltage
+    nexttile
+    plot(d2.v1,d2.("Ix(LP:Plasma)"))
+    title('Current Voltage Curve')
+    xlabel('Bias Voltage [V]')
+    ylabel('Current [A]')
+
+    sgtitle(sprintf('SC area: %2.1fm^{2}',d2.steps(i)))
+
+end
+
+
+%% Attempt to estimate Temperature/ Density
+
+areaarray = unique(data.steps);
+density = 1e12;
+for i = 1:height(areaarray)
+    d2 = data(data.steps == areaarray(i),:);
+    [t,n] = gettempdensityest(d2.appliedpotential,-d2.("Ix(LP:Plasma)"),true,areaarray(i),density);
+    
+    
+    fprintf('Swenson Temp Guess: %d, Density Guess: %d\n',t(2),n(2))
+    fprintf('Swenson Cyl Guess: %d, Density Guess: %d\n',t(3),n(3))
+    fprintf('Debchoudhury Temp Guess: %d, Density Guess: %d\n',t(1),n(1))
+
+end
