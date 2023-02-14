9449ef56dbbc35e489e5bf85eddee474a50bc94b
Jason Powell <32348565+booleansmith@users.noreply.github.com>
2023-02-14 11:10:11 -0700

    an attempt at a gui that may be helpful for understand how plasma parameters interact with eachother

---
+++ DataGeneration/guidemo.m
@@ -0,0 +1,25 @@
+
+% Set the initial values of the second-order dynamic system and create the system model.
+zeta = .5;                           % Damping Ratio
+wn = 2;                              % Natural Frequency
+sys = tf(wn^2,[1,2*zeta*wn,wn^2]); 
+
+% Create a figure for the GUI and configure the axes for displaying the step response.
+f = figure;
+ax = axes('Parent',f,'position',[0.13 0.39  0.77 0.54]);
+h = stepplot(ax,sys);
+setoptions(h,'XLim',[0,10],'YLim',[0,2]);
+
+% Add the slider and slider label text to the figure.
+b = uicontrol('Parent',f,'Style','slider','Position',[81,54,419,23],...
+              'value',zeta, 'min',0, 'max',1);
+bgcolor = f.Color;
+bl1 = uicontrol('Parent',f,'Style','text','Position',[50,54,23,23],...
+                'String','0','BackgroundColor',bgcolor);
+bl2 = uicontrol('Parent',f,'Style','text','Position',[500,54,23,23],...
+                'String','1','BackgroundColor',bgcolor);
+bl3 = uicontrol('Parent',f,'Style','text','Position',[240,25,100,23],...
+                'String','Damping Ratio','BackgroundColor',bgcolor);
+
+% Set the callback that updates the step response plot as the damping ratio slider is moved         
+b.Callback = @(es,ed) updateSystem(h,tf(wn^2,[1,2*(es.Value)*wn,wn^2]));
